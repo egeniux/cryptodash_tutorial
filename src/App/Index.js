c28d27ef17c6f6dfe17dc15a17b4e06fdb47a065
Marrero Almonte <rma09@int.hrs.com>
2019-01-07 12:44:14 +0100

    add addtional tools / yarn

--- src/App/Index.js
+++ src/App/Index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './WelcomeMessage';
+
 
 class App extends Component {
   render() {
     return (
-      <div> Hello World! </div>
+      <Welcome name={"CryptoDash"}/>
     );
   }
 }
