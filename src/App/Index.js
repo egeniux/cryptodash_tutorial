86b638e7d494cc6be7fc7ee16bf0f7b788c1f7a2
Marrero Almonte <rma09@int.hrs.com>
2019-01-07 14:04:18 +0100

    add styled components lib

--- src/App/Index.js
+++ src/App/Index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Welcome from './WelcomeMessage';
+import styled, {css} from 'styled-components';
 
 
 class App extends Component {
