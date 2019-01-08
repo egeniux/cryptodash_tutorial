ada74341f96c24154a527ef9787e6eb0aefbc53b
Marrero Almonte <rma09@int.hrs.com>
2019-01-08 15:30:20 +0100

    add prices component

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -18,18 +18,39 @@ export class AppProvider extends React.Component {
             addCoin: this.addCoin,
             removeCoin: this.removeCoin,
             isInFavorites: this.isInFavorites,
-            confirmFavorites: this.confirmFavorites
+            confirmFavorites: this.confirmFavorites,
+            setFilteredCoins: this.setFilteredCoins
         }
 
     }
 
     componentDidMount = () =>{
          this.fetchCoins();
+         this.fetchPrices();
     }
 
     fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
         this.setState({coinList});
+    }
+
+    fetchPrices = async () => {
+        if(this.state.firstVisit) return;
+        let prices = await this.prices();
+        this.setState({prices});
+    }
+
+    prices = async () => {
+        let returnData = [];
+        for(let i=0; i < this.state.favorites.lenght; i++){
+            try {
+                let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
+                returnData.push(priceData);
+            } catch (e){
+                console.warn('Fetch price error: ', e);
+            }
+        }
+        return returnData;
     }
 
     addCoin = key => {
@@ -67,6 +88,8 @@ export class AppProvider extends React.Component {
     }
 
     setPage = page => this.setState({page})
+
+    setFilteredCoins = (filteredCoins) => this.setState({filteredCoins})
 
     render(){
         return(
--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { backgroundColor2, fontSize2 } from '../Shared/Styles';
+import { AppContext } from '../App/AppProvider';
+import _ from 'lodash';
+import fuzzy from 'fuzzy';
 
 const SearchGrid = styled.div`
     display: grid;
@@ -11,16 +14,47 @@ const SearchInput = styled.input`
     ${backgroundColor2}
     ${fontSize2}
     border: 1px solid;
+    background: white;
     height: 25px;
     color: #1163c9;
     place-self: center left;
 `
+const handleFilter = _.debounce((inputValue, coinList, setFilterCoins) => {
+    
+    let coinSymbols = Object.keys(coinList);
+
+    let coinNames = coinSymbols.map(sym => coinList[sym].CoinName)
+    let allStringToSearch = coinSymbols.concat(coinNames);
+    let fuzzyResults = fuzzy
+                        .filter(inputValue, allStringToSearch, {})
+                        .map(result => result.string);
+
+    let filteredCoins = _.pickBy(coinList, (result, symKey) => {
+        let coinName = result.CoinName;
+        return (_.includes(fuzzyResults, symKey) || _.includes(fuzzyResults, coinName));
+    });
+    
+    setFilterCoins(filteredCoins);
+}, 500);
+
+function filterCoins(e, setFilteredCoins, coinList){
+    let inputValue = e.target.value;
+    if (!inputValue){
+        setFilteredCoins(null);
+        return;
+    }
+    handleFilter(inputValue, coinList, setFilteredCoins);
+}
 
 export default function(){
     return (
-        <SearchGrid> 
-            <h3> Search all coins </h3> 
-            <SearchInput />
-        </SearchGrid>
+        <AppContext.Consumer>
+            {({setFilteredCoins, coinList}) =>
+            <SearchGrid> 
+                <h3> Search all coins </h3> 
+                <SearchInput onKeyUp={(e)=> filterCoins(e, setFilteredCoins, coinList)}/>
+            </SearchGrid>
+            }
+        </AppContext.Consumer>
     );
 }
