744b0705df97ccd4798d07df0700894d243fd4b3
Marrero Almonte <rma09@int.hrs.com>
2019-01-08 12:51:22 +0100

    Add Search sections

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,14 +3,16 @@ import Welcome from '../Settings/WelcomeMessage';
 import ConfirmButtom from './ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
+import Search from './Search';
 
 export default function(){
     return (
         
-            <Page name="Settings">
+            <Page name="Settings" >
                 <Welcome name={"CryptoDash"}/> 
                 <CoinGrid topSection/>
                 <ConfirmButtom/>
+                <Search/>
                 <CoinGrid/>
             </Page>
     )
