d3ea75061cd30c065b18043b4f6f4e55a7808d5e
ramon <egeniux@gmail.com>
2019-01-14 16:17:25 +0100

    add Highcharts

---
+++ src/Dashboard/PriceChart.js
@@ -0,0 +1,21 @@
+import HighchartsConfig from './HighchartsConfig';
+import React from 'react';
+import {Tile} from '../Shared/Tile';
+import {AppContext} from '../App/AppProvider';
+import ReactHightcharts from 'react-highcharts';
+
+
+export default function () {
+  return (
+      <AppContext.Consumer>
+          {({}) => 
+          <Tile>
+              <ReactHightcharts config={HighchartsConfig()}/>
+          </Tile>
+          }
+      </AppContext.Consumer>
+  )
+}
+
+
+
